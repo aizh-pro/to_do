d2a9f483f668c6bddc0f4391c2f363597c1d1403
Aitkeeva Aizhan <aitkeeva.aizhan@gmail.com>
2020-07-26 03:05:52 +0600

    Added admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import Task
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'deadline', 'created_at']
+    list_filter = ['status']
+    search_fields = ['title', 'status']
+    fields = ['title', 'status', 'deadline', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Task)
+
