730181ca5da8f5d829bcc089ead7301243320534
Aitkeeva Aizhan <aaitkeeva@beeline.kz>
2020-07-26 00:08:55 +0600

    Base code of project

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('new','Новая'),
+    ('in progress','В процессе'),
+    ('done','Сделано')
+]
+
+class Task(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Описание')
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Новая')
+    deadline = models.DateField(null=True, blank=True, verbose_name='Дедлайн')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.title)
+
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index_view(request):
+    print(request.GET.getlist('author'))
+    return render(request, 'index.html')
+
+
+def task_create_view(request):
+    if request.method == 'GET':
+        return render(request, 'task_create.html')
+    elif request.method == 'POST':
+        context = {
+            'title': request.POST.get('title'),
+            'status': request.POST.get('status'),
+            'deadline': request.POST.get('deadline')
+        }
+        return render(request, 'task_view.html', context)
+
