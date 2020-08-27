bfeb75d28341dd5c7aad246d25749f57aecd9d07
Aitkeeva Aizhan <aitkeeva.aizhan@gmail.com>
2020-08-27 18:17:52 +0600

    Исправлены urls

--- source/to_do_list/urls.py
+++ source/to_do_list/urls.py
@@ -20,11 +20,11 @@ from webapp.views import IndexView, TaskView, TaskUpdateView, TaskDeleteView, Pr
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
+    path('tasks', IndexView.as_view(), name='index'),
     path('task/<int:pk>/', TaskView.as_view(), name='task_view'),
     path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
     path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
-    path('projects/', ProjectListView.as_view(), name='project_list'),
+    path('', ProjectListView.as_view(), name='project_list'),
     path('project/<int:pk>/', ProjectView.as_view(), name='project_view'),
     path('project/add/', ProjectCreateView.as_view(), name='project_create'),
     path('project/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
