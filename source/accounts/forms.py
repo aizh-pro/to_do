c42279c87b2440ffc1cfa5a4e18b3e40cb1aca0d
Aitkeeva Aizhan <aitkeeva.aizhan@gmail.com>
2020-09-08 19:04:40 +0600

    Required Email

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,8 +1,10 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
 
 class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2',
                   'first_name', 'last_name', 'email']
