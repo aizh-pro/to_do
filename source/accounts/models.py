fa0aeccbd1ecfd3366ad693b2e9d802faffeacb0
Aitkeeva Aizhan <aitkeeva.aizhan@gmail.com>
2020-09-14 15:16:27 +0600

    Добавление Profile модели в accounts

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Profile(models.Model):
+    user: AbstractUser = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
+    user_info = models.TextField(null=True, blank=True, verbose_name='О себе')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    profile = models.URLField(null=True, blank=True, verbose_name='Профиль ссылка')
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
